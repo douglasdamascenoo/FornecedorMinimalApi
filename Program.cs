6460156d74ad546dc4fbfd3ebc8e4e68449fbf29
Douglas Damasceno <douglasddx@outlook.com>
2024-01-15 23:18:01 -0300

    Criação dos verbos PUT e DELETE

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using minimalFornecedor.Data;
 using Microsoft.EntityFrameworkCore;
 using minimalFornecedor.Models;
+using MiniValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -45,13 +46,68 @@ app.MapPost("/fornecedor", async (
     Fornecedor fornecedor
     ) =>
     {
+        if (!MiniValidator.TryValidate(fornecedor, out var errors))
+            return Results.ValidationProblem(errors); //HTTP 400
+
         context.Fornecedores.Add(fornecedor);
         var result = await context.SaveChangesAsync();
+
+        return result > 0
+            //? Results.Created($"/forncedor/{fornecedor.Id}", fornecedor)
+            ? Results.CreatedAtRoute("GetFornecedorPorId", new { id = fornecedor.Id }, fornecedor)
+            : Results.BadRequest("Houve um problema ao salvar o registro");
     })
+.ProducesValidationProblem()
 .Produces<Fornecedor>(StatusCodes.Status201Created)
 .Produces(StatusCodes.Status400BadRequest)
 .WithName("PostFornecedor")
 .WithTags("Fornecedor");
 
+app.MapPut("/fornecedor/{id}", async (
+    Guid id,
+    FornecedorContextDb context,
+    Fornecedor fornecedor
+    ) =>
+    {
+        var fornecedorBanco =
+            await context.Fornecedores.AsNoTracking<Fornecedor>().FirstOrDefaultAsync(q => q.Id == id);
+        if (fornecedorBanco == null) return Results.NotFound();
+
+        if (!MiniValidator.TryValidate(fornecedor, out var errors))
+            return Results.ValidationProblem(errors);
+
+        context.Fornecedores.Update(fornecedor);
+        var result = await context.SaveChangesAsync();
+
+        return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao salvar o registro");
+    })
+    .ProducesValidationProblem()
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PutFornecedor")
+    .WithTags("Fornecedor");
+
+app.MapDelete("/fornecedor/{id}", async (
+    Guid id,
+    FornecedorContextDb context
+    ) =>
+    {
+        var fornecedor = await context.Fornecedores.FindAsync(id);
+        if (fornecedor == null) return Results.NotFound();
+
+        context.Fornecedores.Remove(fornecedor);
+        var result = await context.SaveChangesAsync();
+
+        return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao deletar o registro");
+    })
+    .Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("DeleteFornecedor")
+    .WithTags("Fornecedor");
 
 app.Run();
