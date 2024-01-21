4e9d310f67b02f4c95dc3b1252d1a69b109a2dc1
Douglas Damasceno <douglasddx@outlook.com>
2024-01-21 00:59:36 -0300

    add filtros de autorização nos endpoints

--- Program.cs
+++ Program.cs
@@ -2,15 +2,27 @@ using minimalFornecedor.Data;
 using Microsoft.EntityFrameworkCore;
 using minimalFornecedor.Models;
 using MiniValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using NetDevPack.Identity.Jwt;
+using NetDevPack.Identity.Model;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddIdentityEntityFrameworkContextConfiguration(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("minimalFornecedor")));
+
 builder.Services.AddDbContext<FornecedorContextDb>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddIdentityConfiguration();
+builder.Services.AddJwtConfiguration(builder.Configuration, "AppSettings");
 
 var app = builder.Build();
 
@@ -20,18 +32,95 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//app.UseAuthConfiguration();
 app.UseHttpsRedirection();
 
-app.MapGet("/fornecedor", async (
+app.MapPost("/registro", [AllowAnonymous] async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppJwtSettings> appJwtSettings,
+    RegisterUser registerUser) =>
+    {
+        if (registerUser == null)
+            return Results.BadRequest("Usuário não informado");
+
+        if (!MiniValidator.TryValidate(registerUser, out var errors))
+            return Results.ValidationProblem(errors);
+        var user = new IdentityUser
+        {
+            UserName = registerUser.Email,
+            Email = registerUser.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(user, registerUser.Password);
+
+        if (!result.Succeeded)
+            return Results.BadRequest(result.Errors);
+
+        var jwt = new JwtBuilder()
+            .WithUserManager(userManager)
+            .WithJwtSettings(appJwtSettings.Value)
+            .WithEmail(user.Email)
+            .WithJwtClaims()
+            .WithUserClaims()
+            .WithUserRoles()
+            .BuildUserResponse();
+
+        return Results.Ok(jwt);
+    })
+.ProducesValidationProblem()
+.Produces(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("RegistroUsuario")
+.WithTags("Usuario");
+
+app.MapPost("/login", [AllowAnonymous] async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppJwtSettings> appJwtSettings,
+    LoginUser loginUser) =>
+    {
+        if (loginUser == null)
+            return Results.BadRequest("Usuário não informado");
+
+        if (!MiniValidator.TryValidate(loginUser, out var errors))
+            return Results.ValidationProblem(errors);
+
+        var result = await signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+        if (result.IsLockedOut)
+            return Results.BadRequest("Usuário bloqueado");
+
+        if (!result.Succeeded)
+            return Results.BadRequest("Usuário ou senha inválidos");
+
+        var jwt = new JwtBuilder()
+                    .WithUserManager(userManager)
+                    .WithJwtSettings(appJwtSettings.Value)
+                    .WithEmail(loginUser.Email)
+                    .WithJwtClaims()
+                    .WithUserClaims()
+                    .WithUserRoles()
+                    .BuildUserResponse();
+
+        return Results.Ok(jwt);
+
+    }).ProducesValidationProblem()
+      .Produces(StatusCodes.Status200OK)
+      .Produces(StatusCodes.Status400BadRequest)
+      .WithName("LoginUsuario")
+      .WithTags("Usuario");
+
+app.MapGet("/fornecedor", [AllowAnonymous] async (
     FornecedorContextDb context) =>
     await context.Fornecedores.ToListAsync())
 .WithName("GetFornecedor")
 .WithTags("Fornecedor");
 
-app.MapGet("/fornecedor/{id}", async (
+app.MapGet("/fornecedor/{id}", [AllowAnonymous] async (
     Guid id,
-    FornecedorContextDb context
-    ) =>
+    FornecedorContextDb context) =>
     await context.Fornecedores.FindAsync(id)
     is Fornecedor fornecedor
         ? Results.Ok(fornecedor)
@@ -41,10 +130,9 @@ app.MapGet("/fornecedor/{id}", async (
 .WithName("GetFornecedorPorId")
 .WithTags("Fornecedor");
 
-app.MapPost("/fornecedor", async (
+app.MapPost("/fornecedor", [Authorize] async (
     FornecedorContextDb context,
-    Fornecedor fornecedor
-    ) =>
+    Fornecedor fornecedor) =>
     {
         if (!MiniValidator.TryValidate(fornecedor, out var errors))
             return Results.ValidationProblem(errors); //HTTP 400
@@ -63,11 +151,10 @@ app.MapPost("/fornecedor", async (
 .WithName("PostFornecedor")
 .WithTags("Fornecedor");
 
-app.MapPut("/fornecedor/{id}", async (
+app.MapPut("/fornecedor/{id}", [Authorize] async (
     Guid id,
     FornecedorContextDb context,
-    Fornecedor fornecedor
-    ) =>
+    Fornecedor fornecedor) =>
     {
         var fornecedorBanco =
             await context.Fornecedores.AsNoTracking<Fornecedor>().FirstOrDefaultAsync(q => q.Id == id);
@@ -89,10 +176,9 @@ app.MapPut("/fornecedor/{id}", async (
     .WithName("PutFornecedor")
     .WithTags("Fornecedor");
 
-app.MapDelete("/fornecedor/{id}", async (
+app.MapDelete("/fornecedor/{id}", [Authorize] async (
     Guid id,
-    FornecedorContextDb context
-    ) =>
+    FornecedorContextDb context) =>
     {
         var fornecedor = await context.Fornecedores.FindAsync(id);
         if (fornecedor == null) return Results.NotFound();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # minimalApi
-Minimal API 
+## Fornecedor Minimal API
+1. Migration Add Initial Version Context
+> dotnet ef migrations add initial-jwt-context --context NetDevPackAppDbContext
+2. Update Database Context
+> dotnet ef database update --context NetDevPackAppDbContext
